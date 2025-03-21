675ce6eba6b005a76e2cfa5c7f23f88b04755624
Merin Mathew <merinphilaminmathew19@gmail.com>
2025-03-21 15:44:25 +0530

    frontend .env added

--- auralink_frontend/src/pages/MainPages/LoginPage.jsx
+++ auralink_frontend/src/pages/MainPages/LoginPage.jsx
@@ -1,10 +1,19 @@
-import Login from '@/components/site/Login'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Login from '@/components/site/Login';
 
 function LoginPage() {
-  return (
-    <Login/>
-  )
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user?.logged_in) {
+      navigate('/home', { replace: true });
+    }
+  }, [user, navigate]);
+
+  return <Login />;
 }
 
-export default LoginPage
+export default LoginPage;
